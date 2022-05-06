0e43ea9768a0c962398e2e767d9b950c1283740e
daniel <daniel2mf@yahoo.com.br>
2022-05-06 02:20:43 -0300

    api rest emporium store

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,5 @@
 import { AuthModule } from './auth/auth.module';
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
 
 import { MongooseModule } from '@nestjs/mongoose';
@@ -19,7 +17,7 @@ import { ConfigModule } from '@nestjs/config';
     ProductsModule,
     UsersModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
