0b2de052429a0d297b10f39e89daccc7bc9a4ace
daniel <daniel2mf@yahoo.com.br>
2022-05-06 01:49:52 -0300

    api rest emporium store

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,11 +7,14 @@ import { ProductsModule } from './products/products.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UsersModule } from './users/users.module';
 
+import { ConfigModule } from '@nestjs/config';
+
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     AuthModule,
     MongooseModule.forRoot(
-      'mongodb+srv://danielap:2022@cluster0.pljin.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+      `mongodb+srv://${process.env.DATABASE_USER}:${process.env.DATABASE_PASSWORD}@cluster0.pljin.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`,
     ),
     ProductsModule,
     UsersModule,
